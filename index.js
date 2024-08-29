6e5f525c55d64dccc87e1057986eb35d46a4e217
Aparna02sajeevan <151947437+Aparna02sajeevan@users.noreply.github.com>
2024-08-29 23:07:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-var RandomNumber =Math.random(Math.random()*7);
+var RandomNumber1 =Math.floor(Math.random()*6)+1;
+var dicerandom1 ="dice" + RandomNumber1+ ".jpg";
+var images1 = document.querySelectorAll("img") [0].setAttribute("src",dicerandom1);
 
-var dicerandom="dice" + RandomNumber + ".jpg";
 
-var source = "images/" +dicerandom;
-var images1 = document.querySelectorAll("img") [2].setAttribute("src",source);
 
+var RandomNumber2 =Math.floor(Math.random()*6)+1;
+var dicerandom2 ="dice" + RandomNumber2+ ".jpg";
+var images1 = document.querySelectorAll("img") [1].setAttribute("src",dicerandom2);
+
+if (RandomNumber1>RandomNumber2)
+{
+    document.querySelector("h1").innerHTML ="🚩  Player 1 Wins!";
+}
+else if(RandomNumber1<RandomNumber2)
+{
+    document.querySelector("h1").innerHTML ="🚩 Player 2 Wins! ";
+}
+else {
+
+    document.querySelector("h1").innerHTML =" DRAW 🏳";
+}
